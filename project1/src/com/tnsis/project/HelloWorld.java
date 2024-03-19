a297a8d9ad3ce1ee79466ed5a1c27372551043b4
ndgop <ndgop@LAPTOP-DK88PU5F>
2024-03-19 21:36:33 +0530

    First commit

---
+++ project1/src/com/tnsis/project/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.tnsis.project;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("HelloWorld");
+	}
+
+}
